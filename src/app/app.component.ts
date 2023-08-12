cdc865344c5328e92c23f42d15890781fc4cfa9a
Stanlys <3995782+stanlys@users.noreply.github.com>
2023-08-12 20:16:21 +0300

    feat: install google sheet db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,22 @@ export class AppComponent {
   endDate: Date = new Date();
   maxDate: Date = new Date(new Date().setDate(new Date().getDate() + 14));
 
+  loading = false;
+  formData: any = {};
+
   helloWorld() {
     console.log(
       ` start ${dayjs(this.startDate).format('MM / DD / YYYY')}  | end ${dayjs(
         this.endDate
       ).format('MM * DD * YYYY')}`
     );
+  }
+
+  onCreateAccountClick() {}
+
+  async onSubmit(e: Event) {
+    e.preventDefault();
+    const { email, password } = this.formData;
+    this.loading = true;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DxButtonModule, DxDateRangeBoxModule } from 'devextreme-angular';
+import {
+  DxBoxModule,
+  DxButtonModule,
+  DxDateRangeBoxModule,
+  DxFormModule,
+} from 'devextreme-angular';
+import { LoginFormModule } from './components/login-form/login-form.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, DxButtonModule, AppRoutingModule, DxDateRangeBoxModule],
+  imports: [
+    BrowserModule,
+    DxButtonModule,
+    AppRoutingModule,
+    DxDateRangeBoxModule,
+    LoginFormModule,
+    DxBoxModule,
+    DxFormModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+// AIzaSyA_fXye0_JWXfRQLwBSIIcFc-WsZOnBz3g
